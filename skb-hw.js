40596bbb01f7bfa588e817b330957f95b9667827
siobhankb <siobhan.boylan@gmail.com>
2022-06-15 02:44:40 -0500

    stuck on q3

--- skb-hw.js
+++ skb-hw.js
@@ -10,12 +10,11 @@
     Ex 3. toCelcius(50) // 10
 */
 var toCelsius = (Fahrenheit) => {
-    return (Fahrenheit-32) * (5/9)
-}
+  return (Fahrenheit - 32) * (5 / 9);
+};
 
 // let cTemp = toCelsius(32)
 // console.log(cTemp)
-
 
 /*
     Exercise 2: Create a class for a User that sets username and password property
@@ -29,35 +28,36 @@ var toCelsius = (Fahrenheit) => {
 //         console.log(`${this.name} is eating`)
 //     },
 const userMethods = {
-    changePassword: function (oldPassword, newPassword) {
-        if (this.password == oldPassword) {
-            this.password == newPassword
-            console.log(`${this.username}'s password changed to ${newPassword}`)
-        };
+  changePassword: function (oldPassword, newPassword) {
+    if (this.password == oldPassword) {
+      this.password == newPassword;
+      console.log(`${this.username}'s password changed to ${newPassword}`);
     }
+  },
+};
+
+function User(username, password) {
+  let user = {};
+  user.username = username;
+  user.password = password;
+  user.changePassword = userMethods.changePassword;
+  return user;
 }
 
-function User(username, password){
-    let user = {}
-    user.username = username
-    user.password = password
-    user.changePassword = userMethods.changePassword;
-    return user
-}
-
-let mememe = User('allaboutme', 'memyselfi')
-console.log(`mememe is ${mememe.username}`)
+let mememe = User("allaboutme", "memyselfi");
+console.log(`mememe is ${mememe.username}`);
 console.log(`mememe old password is ${mememe.password}`);
 console.log(`=====================================`);
 
-mememe.changePassword('memyselfi', 'centeroftheuniverse')
+mememe.changePassword("memyselfi", "centeroftheuniverse");
 console.log(`mememe's new password is ${mememe.password}`);
 
 console.log(`=====================================`);
 
 //WHY WON'T IT CHANGE AGAIN?
-mememe.changePassword('centeroftheuniverse', 'xxx');
+mememe.changePassword("centeroftheuniverse", "xxx");
 console.log(`mememe's new password is ${mememe.password}`);
+
 /*
     Exercise 3: Write a function that takes in an array of integers and returns
     an array of the negative integers squared using Array methods
@@ -66,3 +66,17 @@ console.log(`mememe's new password is ${mememe.password}`);
     Ex 2. squareNegatives([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]) // [25, 16, 9, 4, 1]
     Ex 3. squareNegatives([2, 4, 6, 8, 10]) // []
 */
+
+// let max = (a, b) => a > b ? a:null;
+// let giggle = max(16, 19)
+// console.log(giggle)
+
+const arr1 = [-10, -3, 4, -2, 8, 9, -3];
+console.log(arr1);
+const squaresOfNegatives = function (arr) {
+  let squaredArr = [];
+  let squareNegatives = arr1.map((num) => (num < 0 ? sqaredArr.push(num) : 0));
+  console.log(squaredArr);
+  return sqaredArr;
+};
+console.log(squareNegatives);
